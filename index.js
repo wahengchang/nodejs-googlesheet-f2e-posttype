a3ba0b9e1656e359016a37546729e3bcdc8fbc98
waheng <wahengchang@gmail.com>
2023-03-17 16:25:22 +0800

    added SEO feature

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 (async function(){
     const express = require('express')
     const app = express()
+    const sitemap = require('./lib/sitemap')
     
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-= handlebars: start -=-=-=-=-=-=-=-=-=-=-=-=-=-=
   const exphbs = require('express-handlebars');
@@ -11,6 +12,13 @@
   app.engine('handlebars', hbs.engine);
   app.set('view engine', 'handlebars');
   // -=-=-=-=-=-=-=-=-=-=-=-=-=-= handlebars: end -=-=-=-=-=-=-=-=-=-=-=-=-=-=
+  app.get('/sitemap.xml', (req,res) => {
+    const subSitemap = [
+      '/jobs/sitemap.xml'
+    ]
+    res.header('Content-Type', 'application/xml');
+    return res.send(sitemap.sitemapList(subSitemap, req))
+  })
 
   app.use('/jobs', await require('./routers/post')())
 
--- routers/post/index.js
+++ routers/post/index.js
@@ -1,11 +1,13 @@
-
 const express = require("express");
 const Service = require('./Service')
+const { SitemapStream, streamToPromise } = require('sitemap');
+const { Readable } = require('stream');
+
 const router = express.Router();
 
 const service = new Service('jobs')
 
-module.exports = async function() {
+module.exports = async function () {
   await service.init()
 
   router.get("/", async function (req, res) {
@@ -16,7 +18,7 @@ module.exports = async function() {
       const size = req.query.size || 999
       const filterBy = req.query.filterBy;
       const filterByValue = req.query.filterByValue;
-      
+
       const items = service.queryPost({
         orderByColumn,
         orderBy,
@@ -32,23 +34,57 @@ module.exports = async function() {
         posttype: '',
       })
     }
-    catch(e) {
+    catch (e) {
       console.error(e)
       return res.send('404')
     }
   });
 
+  router.get("/sitemap.xml", async function (req, res) {
+    try {
+      const host = req.get('host');
+      const protocol = req.protocol
+      let sitemap;
+
+      res.header('Content-Type', 'application/xml');
+
+      let changefreq = 'daily';
+      const items = service.queryPost({
+        page: 1,
+        size: 9999,
+      })
+      let links = items.map(item => {
+        return {
+          url: item.uri,
+          changefreq,
+          priority: 0.9
+        }
+      })
+
+      const stream = new SitemapStream({ hostname: `${protocol}://${host}`, lastmodDateOnly: true })
+      return streamToPromise(Readable.from(links).pipe(stream)).then((data) => {
+        sitemap = data; // Cache the generated sitemap
+        stream.end();
+        return res.status(200).send(data.toString())
+      });
+    }
+    catch (e) {
+      console.error(e)
+      return res.send('404')
+    }
+  })
+
   router.get("/:id", async function (req, res) {
     try {
       const id = req.params.id;
-  
+
       const data = service.getPost(id)
-      
+
       return res.render('single.handlebars', {
         ...data
       })
     }
-    catch(e) {
+    catch (e) {
       console.error(e)
       return res.send('404')
     }
